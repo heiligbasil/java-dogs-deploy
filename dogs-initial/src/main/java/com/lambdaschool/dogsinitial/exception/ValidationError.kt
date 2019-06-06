09e6c06442094a2c994de2629e1c38b622327674
heiligbasil <basilief@yahoo.com>
2019-06-06 13:20:15 -0700

    Added more dependencies

--- dogs-initial/src/main/java/com/lambdaschool/dogsinitial/exception/ValidationError.kt
+++
@@ -1,3 +0,0 @@
-package com.lambdaschool.dogsinitial.exception
-
-data class ValidationError(var code: String? = null, var message: String? = null)
--- dogs-initial/src/main/java/com/lambdaschool/dogsinitial/java/CheckDogJava.java
+++
@@ -1,6 +0,0 @@
-package com.lambdaschool.dogsinitial.java;
-
-public interface CheckDogJava
-{
-    boolean test(DogJava d);
-}
